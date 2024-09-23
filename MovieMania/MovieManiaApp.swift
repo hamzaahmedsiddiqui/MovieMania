93ff4c48d12a805a9671413598b34a5e0fdd5eca
hamzaahmedsiddiqui <dev.hamxa@gmail.com>
2024-09-24 00:27:16 +0200

    Initial commit

---
+++ MovieMania/MovieManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieManiaApp.swift
+//  MovieMania
+//
+//  Created by hamza Ahmed on 2024-09-24.
+//
+
+import SwiftUI
+
+@main
+struct MovieManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
